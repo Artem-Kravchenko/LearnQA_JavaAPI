6122d1913733e736d90e473879b464bbd86efa74
artem_kravchenko <sam_glad@mail.ru>
2022-12-14 17:00:03 +0300

    StringLength (Задание №10)

---
+++ src/test/java/test/StringLength.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class StringLength {
+    String name = "ABC";
+
+
+    @Test
+    public void testStringLength(){
+        assertTrue(this.name.length() > 15, "String length less than 15 symbols");
+        }
+
+}
+
